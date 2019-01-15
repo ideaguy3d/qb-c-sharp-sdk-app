0042c682f44e696defc88b313a57701059588531
Julius Alvarado <javascript.uiux@gmail.com>
2019-01-14 17:26:45 -0800

    copied and pasted PurcahseOrderQueryAdd code to see if it works

--- qbInvoiceProgram1/invoiceQuery/InvoiceQuery.cs
+++ qbInvoiceProgram1/invoiceQuery/InvoiceQuery.cs
@@ -1,12 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO; 
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using Interop.QBXMLRP2; 
 
 namespace invoiceQuery
 {
-    class Program
+    class InvoiceQuery
     {
 
         static void Main(string[] args)
--- qbInvoiceProgram1/invoiceQueryNetV2/Program.cs
+++ qbInvoiceProgram1/invoiceQueryNetV2/Program.cs
@@ -1,20 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Interop.QBXMLRP2; 
 
 namespace invoiceQueryNetV2
 {
-    class Program
+    class QueryInvoicesPrac
     {
+
+        private static string PurchaseOrderAdd_AddXml()
+        {
+            string qbXml = "xml";
+
+            return qbXml; 
+        }
 
         static void Main(string[] args)
         {
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
         }
 
     }
