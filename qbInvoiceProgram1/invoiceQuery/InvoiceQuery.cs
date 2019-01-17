dfd5f2b543ac0108f1a872cf8611e18c844563b0
Julius Alvarado <javascript.uiux@gmail.com>
2019-01-16 17:41:01 -0800

    very basic invoice query built

--- qbInvoiceProgram1/invoiceQuery/InvoiceQuery.cs
+++ qbInvoiceProgram1/invoiceQuery/InvoiceQuery.cs
@@ -6,9 +6,15 @@ using Interop.QBXMLRP2;
 
 namespace RedstoneQuickBooks
 {
-    class InvoiceQuery
+    class MessageSetRq
     {
-        private string logLocation = @"C:\Temp\PurchaseOrderAddRequest.xml";
+        private static string appName = "Redstone Print and Mail Data Engineering";
+
+        private static string logLocation = @"C:\Temp\PurchaseOrderAddRequest.xml";
+        // C:\xampp\htdocs\qb
+        private static string logBase = @"C:\xampp\htdocs\qb\";
+        private static string logInvoiceQueryDataLocation = @"C:\xampp\htdocs\qb\invoice-query\InvoiceQueryData.xml";
+        private static string logInvoiceQueryError = @"C:\xampp\htdocs\qb\invoice-query\InvoiceQueryData.xml";
 
         private static void LogTxtData(string filePath, string strTxt)
         {
@@ -17,6 +23,45 @@ namespace RedstoneQuickBooks
             sw.Flush();
             sw.Close();
         }
+
+        private static void LogQuickBooksData(string filePath, string dataStr)
+        {
+            StreamWriter sw = new StreamWriter(filePath);
+            sw.WriteLine(dataStr);
+            sw.Flush();
+            sw.Close(); 
+        }
+
+        private static void WalkInvoiceQueryRs(string response)
+        {
+            XmlDocument responseXmlDoc = new XmlDocument(); 
+        }
+
+        private static string BuildInvoiceQueryRq(XmlDocument doc, XmlElement qbxmlMsgsRq)
+        {
+            // <InvoiceQueryRq>...</InvoiceQueryRq>
+            XmlElement invoiceQueryRq = doc.CreateElement("InvoiceQueryRq");
+            qbxmlMsgsRq.AppendChild(invoiceQueryRq);
+
+            // <IncludeLineItems>...</IncludeLineItems>
+            XmlElement includeLineItems = doc.CreateElement("IncludeLineItems");
+            invoiceQueryRq.AppendChild(includeLineItems);
+            includeLineItems.InnerText = "true";
+
+            // <IncludeLinkedTxns>...</IncludeLinkedTxns
+            XmlElement includeLinkedTxns = doc.CreateElement("IncludeLinkedTxns");
+            invoiceQueryRq.AppendChild(includeLinkedTxns);
+            includeLinkedTxns.InnerText = "true";
+
+            // <OwnerID>...</OwnerID>
+            XmlElement ownerId = doc.CreateElement("OwnderID");
+            invoiceQueryRq.AppendChild(ownerId);
+            ownerId.InnerText = "0";
+
+            string docOuter = doc.OuterXml; 
+            LogQuickBooksData(MessageSetRq.logBase + "invoice-query\\RedstoneInvoiceQuery.xml", docOuter);
+            return docOuter; 
+        } 
 
         private static string PurchaseOrderAdd_AddXml()
         {
@@ -55,7 +100,7 @@ namespace RedstoneQuickBooks
             XmlElement vendorRef = inputXMLDocPurchaseOrder.CreateElement("VendorRef");
             purchaseOrderAdd.AppendChild(vendorRef);
             //vendorRef.AppendChild(inputXMLDocPurchaseOrder.CreateElement("ListID")).InnerText = "0001";
-            // // I think this is referring to "Vendor Name" or "Company Name" from QB
+            //// I think this is referring to "Vendor Name" or "Company Name" from QB
             vendorRef.AppendChild(inputXMLDocPurchaseOrder.CreateElement("FullName")).InnerText = "Spicers LLC";
 
             // <TxnDate>...</TxnDate>
@@ -133,111 +178,207 @@ namespace RedstoneQuickBooks
         static void Main(string[] args)
         {
             Console.WriteLine("__>> Will start to do purchase order");
+
+            // qb ops controls
+            bool doPurchaseOrderAdd = false;
+            bool doInvoiceQuery = true; 
+
+            // qbxmlrp2 vars
             RequestProcessor2 qbRequestProcessor;
             string ticket = null;
             string purchaseOrderResponse = null;
             string purchaseOrderInput = null;
 
+            /* - Main function wide xml vars - */
+            bool sessionBegun = false;
+            bool connectionOpen = false;
+            RequestProcessor2 rp = null;
+            XmlDocument reqXmlDoc = null;
+            XmlElement qbxml = null; // aka "outer"
+            XmlElement qbxmlMsgsRq = null; // aka "inner" 
+            string responseStr = null;
+            string reqXmlDocOuter = null; 
 
             //------------------------------------------------------------
-            // to do a "PurchaseOrderAdd" op
+            // to set function wide starting vars
             try
             {
-                //-- do the qbXMLRP request
-                qbRequestProcessor = new RequestProcessor2();
-                qbRequestProcessor.OpenConnection("", "Redstone Print and Mail Data Engineering");
-                ticket = qbRequestProcessor.BeginSession("", QBFileMode.qbFileOpenDoNotCare);
-                // // VERY IMPORTANT, invoke the function that actually constructs the qbXML
-                purchaseOrderInput = PurchaseOrderAdd_AddXml();
+                rp = new RequestProcessor2(); 
 
-                purchaseOrderResponse = qbRequestProcessor.ProcessRequest(ticket, purchaseOrderInput);
-                LogTxtData(@"C:\Temp\PurchaseOrderAddResponse_object.xml", purchaseOrderResponse);
+                // XML document
+                reqXmlDoc = new XmlDocument();
 
-                if (ticket != null)
-                {
-                    qbRequestProcessor.EndSession(ticket);
-                }
+                // <?xml version="1.0" encoding="utf-8"?>
+                reqXmlDoc.AppendChild(reqXmlDoc.CreateXmlDeclaration("1.0", "utf-8", null));
 
-                qbRequestProcessor.CloseConnection();
+                // <?qbxml version="13.0"?>
+                reqXmlDoc.AppendChild(reqXmlDoc.CreateProcessingInstruction("qbxml", "version=\"13.0\""));
+
+                // <QBXML>...</QBXML>
+                qbxml = reqXmlDoc.CreateElement("QBXML");
+                reqXmlDoc.AppendChild(qbxml);
+
+                // <QBXMLMsgsRq>...</QBXMLMsgsRq>
+                qbxmlMsgsRq = reqXmlDoc.CreateElement("QBXMLMsgsRq");
+                qbxml.AppendChild(qbxmlMsgsRq);
+                qbxmlMsgsRq.SetAttribute("onError", "stopOnError");
             }
             catch (Exception ex)
             {
-                qbRequestProcessor = null;
-                LogTxtData(@"C:\Temp\PurchaseOrderAddRequestError.xml", ex.Message);
+                rp = null;
+                LogQuickBooksData(MessageSetRq.logBase + "invoice-query\\StartingVarsError.xml", ex.Message);
                 return;
             }
 
+            Console.WriteLine("__>> Line 232 ish");
 
             //------------------------------------------------------------
-            // to parse the "PurchaseOrderAdd" response
+            // to build the request message sets
             try
             {
-                XmlDocument outputXmlPurchaseOrderAdd = new XmlDocument();
-                outputXmlPurchaseOrderAdd.LoadXml(purchaseOrderResponse);
-                XmlNodeList qbXmlMsgsRsNodeList = outputXmlPurchaseOrderAdd.GetElementsByTagName("PurchaseOrderAddRs");
+                reqXmlDocOuter = BuildInvoiceQueryRq(reqXmlDoc, qbxmlMsgsRq); 
+            }
+            catch (Exception ex)
+            {
+                rp = null;
+                LogQuickBooksData(MessageSetRq.logBase + "invoice-query\\RequestMessagesError.xml", ex.Message);
+                return; 
+            }
 
-                if (qbXmlMsgsRsNodeList.Count == 1)
+            Console.WriteLine("__>> Line 247 ish");
+
+            //------------------------------------------------------------
+            // to send the MessageSetRequest
+            try
+            {
+                // , QBXMLRPConnectionType.localQBD
+                rp.OpenConnection("", MessageSetRq.appName);
+                connectionOpen = true;
+                ticket = rp.BeginSession("", QBFileMode.qbFileOpenDoNotCare);
+                sessionBegun = true;
+
+                // send request and get response
+                responseStr = rp.ProcessRequest(ticket, reqXmlDocOuter);
+
+                LogQuickBooksData(MessageSetRq.logBase + "invoice-query\\RedstoneInvoiceQueryRaw.xml", responseStr); 
+
+                // end the session and close the connection to quickbooks
+                rp.EndSession(ticket);
+                sessionBegun = false;
+                rp.CloseConnection();
+                connectionOpen = false; 
+
+                //TODO: Walk qbXML response
+            }
+            catch(Exception ex)
+            {
+                rp = null;
+                LogQuickBooksData(MessageSetRq.logBase + "invoice-query\\SendReqError.xml", ex.Message);
+                
+                if (sessionBegun)
                 {
-                    StringBuilder txtMessage = new StringBuilder();
+                    rp.EndSession(ticket);
+                }
 
-                    XmlAttributeCollection rsAttributes = qbXmlMsgsRsNodeList.Item(0).Attributes;
-                    // get statusCode, statusSeverity, statusMessage
-                    string statusCode = rsAttributes.GetNamedItem("statusCode").Value;
-                    string statusSeverity = rsAttributes.GetNamedItem("statusSeverity").Value;
-                    string statusMessage = rsAttributes.GetNamedItem("statusMessage").Value;
-                    txtMessage.AppendFormat(
-                        "statusCode = {0}, statusSeverity = {1}, statusMessage = {2}",
-                        statusCode, statusSeverity, statusMessage
-                    );
+                if(connectionOpen)
+                {
+                    rp.CloseConnection();
+                }
+                
+                return; 
+            }
 
-                    // get PurchaseOrderAddRs > PurchaseOrderRet node
-                    XmlNodeList purchaseOrderAddRsNodeList = qbXmlMsgsRsNodeList.Item(0).ChildNodes;
-                    if (purchaseOrderAddRsNodeList.Item(0).Name.Equals("PurchaseOrderRet"))
+            Console.WriteLine("__>> Line 290 ish");
+
+            //TODO: refactor this to conform to new way to constructing the Request Message Set
+            if (doPurchaseOrderAdd)
+            {
+                //------------------------------------------------------------
+                // to do a "PurchaseOrderAdd" op
+                try
+                {
+                    //-- do the qbXMLRP request
+                    qbRequestProcessor = new RequestProcessor2();
+                    qbRequestProcessor.OpenConnection("", "Redstone Print and Mail Data Engineering");
+                    ticket = qbRequestProcessor.BeginSession("", QBFileMode.qbFileOpenDoNotCare);
+                    ////-- VERY IMPORTANT, invoke the function that actually constructs the qbXML
+                    purchaseOrderInput = PurchaseOrderAdd_AddXml();
+
+                    purchaseOrderResponse = qbRequestProcessor.ProcessRequest(ticket, purchaseOrderInput);
+                    LogTxtData(@"C:\Temp\PurchaseOrderAddResponse_object.xml", purchaseOrderResponse);
+
+                    if (ticket != null)
                     {
-                        XmlNodeList purchaseOrderAddRetNodeList = purchaseOrderAddRsNodeList.Item(0).ChildNodes;
-                        foreach (XmlNode purchaseOrderAddRetNode in purchaseOrderAddRetNodeList)
-                        {
-                            if (purchaseOrderAddRetNode.Name.Equals("TxnID"))
-                            {
-                                txtMessage.AppendFormat(
-                                    "\r\n__>> Purchase_Order_Add TxnID = {0}",
-                                    purchaseOrderAddRetNode.InnerText
-                                );
-                            }
-                            else if (purchaseOrderAddRetNode.Name.Equals("VendorRef"))
-                            {
-                                txtMessage.AppendFormat(
-                                    "\r\n__>> Purchase_Order_Add inner xml = {0}",
-                                    purchaseOrderAddRetNode.InnerXml
-                                );
-                            }
-                        }
+                        qbRequestProcessor.EndSession(ticket);
                     }
 
-                    LogTxtData(@"C:\Temp\PurchaseOrderAddResponse.txt", txtMessage.ToString());
+                    qbRequestProcessor.CloseConnection();
+                }
+                catch (Exception ex)
+                {
+                    qbRequestProcessor = null;
+                    LogTxtData(@"C:\Temp\PurchaseOrderAddRequestError.xml", ex.Message);
+                    return;
+                }
+
+                //------------------------------------------------------------
+                // to parse the "PurchaseOrderAdd" response
+                try
+                {
+                    XmlDocument outputXmlPurchaseOrderAdd = new XmlDocument();
+                    outputXmlPurchaseOrderAdd.LoadXml(purchaseOrderResponse);
+                    XmlNodeList qbXmlMsgsRsNodeList = outputXmlPurchaseOrderAdd.GetElementsByTagName("PurchaseOrderAddRs");
+
+                    if (qbXmlMsgsRsNodeList.Count == 1)
+                    {
+                        StringBuilder txtMessage = new StringBuilder();
+
+                        XmlAttributeCollection rsAttributes = qbXmlMsgsRsNodeList.Item(0).Attributes;
+                        // get statusCode, statusSeverity, statusMessage
+                        string statusCode = rsAttributes.GetNamedItem("statusCode").Value;
+                        string statusSeverity = rsAttributes.GetNamedItem("statusSeverity").Value;
+                        string statusMessage = rsAttributes.GetNamedItem("statusMessage").Value;
+                        txtMessage.AppendFormat(
+                            "statusCode = {0}, statusSeverity = {1}, statusMessage = {2}",
+                            statusCode, statusSeverity, statusMessage
+                        );
+
+                        // get PurchaseOrderAddRs > PurchaseOrderRet node
+                        XmlNodeList purchaseOrderAddRsNodeList = qbXmlMsgsRsNodeList.Item(0).ChildNodes;
+                        if (purchaseOrderAddRsNodeList.Item(0).Name.Equals("PurchaseOrderRet"))
+                        {
+                            XmlNodeList purchaseOrderAddRetNodeList = purchaseOrderAddRsNodeList.Item(0).ChildNodes;
+                            foreach (XmlNode purchaseOrderAddRetNode in purchaseOrderAddRetNodeList)
+                            {
+                                if (purchaseOrderAddRetNode.Name.Equals("TxnID"))
+                                {
+                                    txtMessage.AppendFormat(
+                                        "\r\n__>> Purchase_Order_Add TxnID = {0}",
+                                        purchaseOrderAddRetNode.InnerText
+                                    );
+                                }
+                                else if (purchaseOrderAddRetNode.Name.Equals("VendorRef"))
+                                {
+                                    txtMessage.AppendFormat(
+                                        "\r\n__>> Purchase_Order_Add inner xml = {0}",
+                                        purchaseOrderAddRetNode.InnerXml
+                                    );
+                                }
+                            }
+                        }
+
+                        LogTxtData(@"C:\Temp\PurchaseOrderAddResponse.txt", txtMessage.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    const string customMessage = "JHA - Error while parsing purchase order response: ";
+                    qbRequestProcessor = null;
+                    LogTxtData(@"C:\Temp\PurchaseOrderAddResponseError.xml", customMessage + ex.Message);
+                    return;
                 }
             }
-            catch (Exception ex)
-            {
-                const string customMessage = "JHA - Error while parsing purchase order response: ";
-                qbRequestProcessor = null;
-                LogTxtData(@"C:\Temp\PurchaseOrderAddResponseError.xml", customMessage + ex.Message);
-                return;
-            }
 
-
-            //------------------------------------------------------------
-            // to do an "InvoiceQuery" op
-            try
-            {
-                //
-
-            }
-            catch (Exception ex)
-            {
-                // 
-
-            }
         }
 
     }
